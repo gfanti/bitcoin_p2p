ade98a14f599808264504083133a7b7d3538a410
Giulia Fanti <giuliafanti@vpnpool-130-126-152-62.near.illinois.edu>
2016-06-28 10:42:49 -0500

    added main function

--- main.py
+++ main.py
@@ -5,76 +5,77 @@ from estimators import *
 from utils import *
 import time
 
-
-degrees = xrange(2,3,1)
-trials = 20
-check_ml = True
-
-
-args = parse_arguments()
+if __name__ == "__main__":
+	
+	degrees = xrange(2,3,1)
+	trials = 20
+	check_ml = True
 
 
-accuracies_first = []
-accuracies_ml_line = []
-accuracies_ml = []
-
-# start = time.time()
-# end = start
-for degree in degrees:
-	print 'On degree ', degree
-	count_first = 0
-	count_ml_line = 0
-	count_ml = 0
-
-	for i in range(trials):
-		if (i % 100) == 0:
-			print 'On trial ', i, ' out of ', trials
-		G = RegularTree(degree,degree + 3)
-		G.spread_message()
-		
-		# First spy estimator
-		est_first = FirstSpyEstimator(G)
-		result_first = est_first.estimate_source()
-		acc_first = est_first.compute_accuracy(G.source, result_first)
-		count_first += acc_first
+	args = parse_arguments()
 
 
-		# # ML estimator line
-		# est_ml_line = MLEstimatorLine(G)
-		# result_ml_line = est_ml_line.estimate_source()
-		# acc_ml_line = est_ml_line.compute_accuracy(G.source, result_ml_line)
-		# count_ml_line += acc_ml_line
-		
-		if check_ml:
-			# ML estimator general
-			est_ml = MLEstimatorMP(G, args.verbose)
-			result_ml = est_ml.estimate_source()
-			acc_ml = est_ml.compute_accuracy(G.source, result_ml)
-			count_ml += acc_ml
+	accuracies_first = []
+	accuracies_ml_line = []
+	accuracies_ml = []
 
-		# if not acc_ml_line == acc_ml:
-		# 	break
-		# print 'Trial ', i, ': ', time.time() - end
-		# end = time.time()
-		# print 'Accuracy', acc_ml
+	# start = time.time()
+	# end = start
+	for degree in degrees:
+		print 'On degree ', degree
+		count_first = 0
+		count_ml_line = 0
+		count_ml = 0
 
-	accuracies_first += [float(count_first) / trials]
-	accuracies_ml_line += [float(count_ml_line) / trials]
-	accuracies_ml += [float(count_ml) / trials]
+		for i in range(trials):
+			if (i % 100) == 0:
+				print 'On trial ', i, ' out of ', trials
+			G = RegularTree(degree,degree + 3)
+			G.spread_message()
+			
+			# First spy estimator
+			est_first = FirstSpyEstimator(G)
+			result_first = est_first.estimate_source()
+			acc_first = est_first.compute_accuracy(G.source, result_first)
+			count_first += acc_first
 
-	print 'accuracies, first-spy:', accuracies_first
-	# print 'accuracies, ML line:', accuracies_ml_line
-	print 'accuracies, ML:', accuracies_ml
 
-	if args.write:
-		result_types = ['first-spy accuracy', 'ML accuracy']
-		param_types = ['degrees']
-		results = [[accuracies_first], [accuracies_ml]]
-		params = [[i for i in degrees]]
-		write_results(result_types, results, param_types, params, args.run)
+			# # ML estimator line
+			# est_ml_line = MLEstimatorLine(G)
+			# result_ml_line = est_ml_line.estimate_source()
+			# acc_ml_line = est_ml_line.compute_accuracy(G.source, result_ml_line)
+			# count_ml_line += acc_ml_line
+			
+			if check_ml:
+				# ML estimator general
+				est_ml = MLEstimatorMP(G, args.verbose)
+				result_ml = est_ml.estimate_source()
+				acc_ml = est_ml.compute_accuracy(G.source, result_ml)
+				count_ml += acc_ml
 
-print 'The first-spy estimator accuracy: ', accuracies_first
-print 'The ML estimator accuracy: ', accuracies_ml
-print 'Tested on degrees', degrees
-# end = time.time()
-# print 'The runtime is ', end-start
+			# if not acc_ml_line == acc_ml:
+			# 	break
+			# print 'Trial ', i, ': ', time.time() - end
+			# end = time.time()
+			# print 'Accuracy', acc_ml
+
+		accuracies_first += [float(count_first) / trials]
+		accuracies_ml_line += [float(count_ml_line) / trials]
+		accuracies_ml += [float(count_ml) / trials]
+
+		print 'accuracies, first-spy:', accuracies_first
+		# print 'accuracies, ML line:', accuracies_ml_line
+		print 'accuracies, ML:', accuracies_ml
+
+		if args.write:
+			result_types = ['first-spy accuracy', 'ML accuracy']
+			param_types = ['degrees']
+			results = [[accuracies_first], [accuracies_ml]]
+			params = [[i for i in degrees]]
+			write_results(result_types, results, param_types, params, args.run)
+
+	print 'The first-spy estimator accuracy: ', accuracies_first
+	print 'The ML estimator accuracy: ', accuracies_ml
+	print 'Tested on degrees', degrees
+	# end = time.time()
+	# print 'The runtime is ', end-start
